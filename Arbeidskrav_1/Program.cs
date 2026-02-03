98ac280df5aa24ad569eb5a7dc92bab65625a72b
kristianeo <olsen.kristiane@gmail.com>
2026-02-03 21:26:20 +0100

    temp

--- Arbeidskrav_1/Program.cs
+++ Arbeidskrav_1/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace Arbeidskrav_1;
+﻿using Spectre.Console;
+
+namespace Arbeidskrav_1;
 
 class Program
 {
     static void Main(string[] args)
     {
+        AnsiConsole.MarkupLine("[green]✓ Build completed successfully[/]");
         CharacterClass.AbilityScoreGenerator();
         CharacterClass.ClassSelection();
         CharacterClass.ChooseClass();
-        CharacterClass.GenerateCharacter();
+        
+        
     }
 }
