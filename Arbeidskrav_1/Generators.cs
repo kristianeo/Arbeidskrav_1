419a5b5905119d309b8053c69991f2220b351cdd
kristianeo <olsen.kristiane@gmail.com>
2026-02-03 11:46:19 +0100

    temp

--- Arbeidskrav_1/Generators.cs
+++ Arbeidskrav_1/Generators.cs
@@ -1,16 +1,16 @@
-namespace Arbeidskrav_1;
-
-public class Generators
-{
-    public static int DiceRoll(int dice, int sides)
-    {
-        int diceRoll = 0;
-        Random random = new Random();
-        for (int d = 0; d < dice; d ++)
-        {
-            diceRoll += random.Next(1, sides+1);
-        }
-        Console.WriteLine(diceRoll);
-        return diceRoll;
-    }
-}
+// namespace Arbeidskrav_1;
+//
+// public class Generators
+// {
+//     public static int DiceRoll(int dice, int sides)
+//     {
+//         int diceRoll = 0;
+//         Random random = new Random();
+//         for (int d = 0; d < dice; d ++)
+//         {
+//             diceRoll += random.Next(1, sides+1);
+//         }
+//         Console.WriteLine(diceRoll);
+//         return diceRoll;
+//     }
+// }
