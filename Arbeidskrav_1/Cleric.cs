be50323a311d35ef369817c5532800f0d90518b4
kristianeo <olsen.kristiane@gmail.com>
2026-02-03 13:10:28 +0100

    Added Ability dict

--- Arbeidskrav_1/Cleric.cs
+++ Arbeidskrav_1/Cleric.cs
@@ -8,6 +8,16 @@ public class Cleric:CharacterClass
     }
 
     private int _diceroll = Diceroll(1, 6);
+    
+    public static Dictionary<string, int> ClericAbilities = new()
+    {
+        { "Strength", 0 },
+        { "Intelligence", 0 },
+        { "Wisdom", 0 },
+        { "Dexterity", 0 },
+        { "Constitution", 0 },
+        { "Charisma", 0 }
+    };
 
     public override string ToString()
     {
