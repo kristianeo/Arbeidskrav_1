e7693f5966d9e658bcc09202879cf2bbcd253bef
kristianeo <olsen.kristiane@gmail.com>
2026-02-04 13:59:54 +0100

    overridden constitution modifier and added AbilityScore dictionaries

--- Arbeidskrav_1/Cleric.cs
+++ Arbeidskrav_1/Cleric.cs
@@ -2,18 +2,24 @@ namespace Arbeidskrav_1;
 
 public class Cleric(string charName) :CharacterClass("Cleric", "Wisdom", charName, 1500, 1, 6)
 {
-   public static Dictionary<string, int> Abilities = new()
+    public override string ConstitutionModifier(CharacterClass character)
     {
-        { "Strength", 0 },
-        { "Intelligence", 0 },
-        { "Wisdom", 0 },
-        { "Dexterity", 0 },
-        { "Constitution", 0 },
-        { "Charisma", 0 }
-    };
-
-    public override string ToString()
-    {
-        return this.ClassName;
+        var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
+        int modifier = Int16.Parse(Modifier(constitutionScore.Value));
+        int hitPoints = DiceRoll(_dice, _sides) - modifier;
+        if (hitPoints < 1)
+        {
+            hitPoints = 1;
+        }
+        
+        return $"Hit Points: {hitPoints} ({_dice}d{_sides} {modifier})";
     }
+
+    public Dictionary<string, int> AbilityScore(Dictionary<string, int> dict)
+    {
+        AbilityScores = dict;
+        return AbilityScores;
+    }
+    public Dictionary<string, int> AbilityScores = new Dictionary<string, int>();
+
 }
--- Arbeidskrav_1/MagicUser.cs
+++ Arbeidskrav_1/MagicUser.cs
@@ -2,18 +2,26 @@ namespace Arbeidskrav_1;
 
 public class MagicUser(string charName) :CharacterClass("Magic User", "Intelligence", charName, 2500, 1, 4)
 {
-    public static Dictionary<string, int> Abilities = new()
+    public override string ConstitutionModifier(CharacterClass character)
     {
-        { "Strength", 0 },
-        { "Intelligence", 0 },
-        { "Wisdom", 0 },
-        { "Dexterity", 0 },
-        { "Constitution", 0 },
-        { "Charisma", 0 }
-    };
-    
-    public override string ToString()
-    {
-        return this.ClassName;
+        var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
+        int modifier = Int16.Parse(Modifier(constitutionScore.Value));
+        int hitPoints = DiceRoll(_dice, _sides) - modifier;
+        if (hitPoints < 1)
+        {
+            hitPoints = 1;
+        }
+        
+        return $"Hit Points: {hitPoints} ({_dice}d{_sides} {modifier})";
     }
+
+    public Dictionary<string, int> AbilityScore(Dictionary<string, int> dict)
+    {
+        AbilityScores = dict;
+        return AbilityScores;
+    }
+    public Dictionary<string, int> AbilityScores = new Dictionary<string, int>();
+
+   
+    
 }
