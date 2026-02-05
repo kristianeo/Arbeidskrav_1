9f47650a8c350f33e376e97b5f7d51c2874a895e
kristianeo <olsen.kristiane@gmail.com>
2026-02-05 20:04:41 +0100

    same

--- Arbeidskrav_1/CharacterClasses/Cleric.cs
+++ Arbeidskrav_1/CharacterClasses/Cleric.cs
@@ -3,16 +3,5 @@ namespace Arbeidskrav_1.CharacterClasses;
 public class Cleric(string charName, Dictionary<string, int> abilityScores) :
     CharacterClass("Cleric", "Wisdom", charName, 1500, 1, 6, abilityScores)
 {
-    public override string ConstitutionModifier()
-    {
-        var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
-        int modifier = short.Parse(Modifier.Modify(constitutionScore.Value));
-        int hitPoints = DiceRoll.RollDice(Dice, Sides) - modifier;
-        if (hitPoints < 1)
-        {
-            hitPoints = 1;
-        }
-        return $"{hitPoints} ({Dice}d{Sides} {modifier})";
-    }
 
 }
