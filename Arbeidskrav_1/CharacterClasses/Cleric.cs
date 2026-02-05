8b74725edffaff569eb84da5b0c4658d6be10604
kristianeo <olsen.kristiane@gmail.com>
2026-02-05 14:22:48 +0100

    cleaned up constitution modifier

--- Arbeidskrav_1/CharacterClasses/Cleric.cs
+++ Arbeidskrav_1/CharacterClasses/Cleric.cs
@@ -3,17 +3,16 @@ namespace Arbeidskrav_1.CharacterClasses;
 public class Cleric(string charName, Dictionary<string, int> abilityScores) :
     CharacterClass("Cleric", "Wisdom", charName, 1500, 1, 6, abilityScores)
 {
-    public override string ConstitutionModifier(CharacterClass character)
+    public override string ConstitutionModifier()
     {
         var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
-        string mod = Modifier.Modify(constitutionScore.Value);
-        int modifier = Int16.Parse(mod);
+        int modifier = short.Parse(Modifier.Modify(constitutionScore.Value));
         int hitPoints = DiceRoll.RollDice(Dice, Sides) - modifier;
         if (hitPoints < 1)
         {
             hitPoints = 1;
         }
-        return $"{hitPoints} ({Dice}d{Sides} {mod})";
+        return $"{hitPoints} ({Dice}d{Sides} {modifier})";
     }
 
 }
