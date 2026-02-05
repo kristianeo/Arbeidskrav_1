46b763077d88c3c0dbbf1c7a36d5b16fe19e5c7d
kristianeo <olsen.kristiane@gmail.com>
2026-02-05 14:22:33 +0100

    Cleaned up ConstitutionModifier

--- Arbeidskrav_1/CharacterClasses/Fighter.cs
+++ Arbeidskrav_1/CharacterClasses/Fighter.cs
@@ -3,17 +3,16 @@ namespace Arbeidskrav_1.CharacterClasses;
 public class Fighter(string charName, Dictionary<string, int> abilityScores) :
     CharacterClass("Fighter", "Strength", charName, 2000, 1, 8, abilityScores)
 {
-    public override string ConstitutionModifier(CharacterClass character)
+    public override string ConstitutionModifier()
     {
         var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
-        string mod = Modifier.Modify(constitutionScore.Value);
-        int modifier = Int16.Parse(mod);
+        int modifier = short.Parse(Modifier.Modify(constitutionScore.Value));
         int hitPoints = DiceRoll.RollDice(Dice, Sides) - modifier;
         if (hitPoints < 1)
         {
             hitPoints = 1;
         }
-        return $"{hitPoints} ({Dice}d{Sides} {mod})";
+        return $"{hitPoints} ({Dice}d{Sides} {modifier})";
     }
 
     
--- Arbeidskrav_1/CharacterClasses/MagicUser.cs
+++ Arbeidskrav_1/CharacterClasses/MagicUser.cs
@@ -3,17 +3,16 @@ namespace Arbeidskrav_1.CharacterClasses;
 public class MagicUser(string charName, Dictionary<string, int> abilityScores) :
     CharacterClass("Magic User", "Intelligence", charName, 2500, 1, 4, abilityScores)
 {
-    public override string ConstitutionModifier(CharacterClass character)
+    public override string ConstitutionModifier()
     {
         var constitutionScore = AbilityScores.FirstOrDefault(s => s.Key == "Constitution");
-        string mod = Modifier.Modify(constitutionScore.Value);
-        int modifier = Int16.Parse(mod);
+        int modifier = short.Parse(Modifier.Modify(constitutionScore.Value));
         int hitPoints = DiceRoll.RollDice(Dice, Sides) - modifier;
         if (hitPoints < 1)
         {
             hitPoints = 1;
         }
-        return $"{hitPoints} ({Dice}d{Sides} {mod})";
+        return $"{hitPoints} ({Dice}d{Sides} {modifier})";
     }
 
 }
