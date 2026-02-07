40c16bd60131b595237a07c7e453da3af8fba68a
kristianeo <olsen.kristiane@gmail.com>
2026-02-07 16:34:01 +0100

    Created a handler for ability scores under average

--- Arbeidskrav_1/Generators/AbilityScoresGenerator.cs
+++ Arbeidskrav_1/Generators/AbilityScoresGenerator.cs
@@ -5,7 +5,7 @@ namespace Arbeidskrav_1.Generators;
 
 public abstract class AbilityScoresGenerator
 {
-   /// <summary>
+    /// <summary>
     /// Generates ability score per ability by rolling 3d6.
     /// Minimum value 3, maximum value 18.
     /// Uses Average(), if average is less than or equal to 8
@@ -13,41 +13,44 @@ public abstract class AbilityScoresGenerator
     /// </summary>
     public static void GenerateAbilityScores() // TODO: Add "roll ability scores"/view characters (after DB implement)
     {
-        TryAgain:
         Console.Clear();
         foreach (string value in CharacterClass.AbilityScores.Keys)
         {
             CharacterClass.AbilityScores[value] = 0;
             CharacterClass.AbilityScores[value] += DiceRoll.RollDice(3, 6);
         }
-        
+
         UserInterface.UserInterface.DisplayAbilityScores();
-        
         if (Average(CharacterClass.AbilityScores) <= 8)
         {
-            var prompt = new SelectionPrompt<string>()
-                .Title("\n[bold red]Your ability scores are below average... [/]" +
-                       "\nWould you like to reroll?")
-                .AddChoices("Yes", "No");
-            var selected = AnsiConsole.Prompt(prompt);
-            AnsiConsole.MarkupLineInterpolated($"\n[blue]You selected[/] {selected} ");
-            
-            if (selected == "Yes")
-            {
-                Console.Clear();
-                AnsiConsole.Status()
-                    .Spinner(Spinner.Known.Arrow)
-                    .Start("Rerolling...", ctx =>
-                    {
-                        Thread.Sleep(1500);
-                    });
-  
-                AnsiConsole.MarkupLine("[green]Rerolled!![/]");
-                Thread.Sleep(1500);
-                goto TryAgain;
-            }
+            UnderAverageHandler();
         }
     }
+
+    private static void UnderAverageHandler()
+    {
+        var prompt = new SelectionPrompt<string>()
+            .Title("\n[bold red]Your ability scores are below average... [/]" +
+                   "\nWould you like to reroll?")
+            .AddChoices("Yes", "No");
+        var selected = AnsiConsole.Prompt(prompt);
+        AnsiConsole.MarkupLineInterpolated($"\n[blue]You selected[/] {selected} ");
+
+        if (selected == "Yes")
+        {
+            Console.Clear();
+            AnsiConsole.Status()
+                .Spinner(Spinner.Known.Arrow)
+                .Start("Rerolling...", ctx => { Thread.Sleep(1500); });
+
+            AnsiConsole.MarkupLine("[green]Rerolled!![/]");
+            Thread.Sleep(1500);
+            GenerateAbilityScores();
+        }
+        
+    }
+        
+   // TODO: add separate method for values below average 
     public static int Average(Dictionary<string, int> dictionary)
     {
         int total = dictionary.Keys.Sum(value => dictionary[value]);
