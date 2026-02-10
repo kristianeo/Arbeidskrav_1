14b2e80eddfb5d1015d716e43405a943619240f4
kristianeo <olsen.kristiane@gmail.com>
2026-02-10 18:26:57 +0100

    made it pretty

--- Arbeidskrav_1/CharacterRepository/CharacterGetter.cs
+++ Arbeidskrav_1/CharacterRepository/CharacterGetter.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using Arbeidskrav_1.CharacterClasses;
 using Spectre.Console;
@@ -8,7 +9,7 @@ public abstract class CharacterGetter
 {
     public static void GetCharacter()
     {
-        AnsiConsole.MarkupLine("[Blue]Let's search for a character![/]");
+        AnsiConsole.MarkupLine("[bold]Let's search for a character![/]\n");
         string name = AnsiConsole.Ask<string>("What is the name of the character?");
         
         const string filePath = @"C:\Users\olsen\OneDrive\Dokumenter\GitHub\Arbeidskrav_1\Arbeidskrav_1\CharacterRepository\CharacterRepo.json";
@@ -36,12 +37,33 @@ public abstract class CharacterGetter
 
     private static void DisplayCharacter(Dictionary<string, string> info)
     {
-        foreach (string key in info.Keys)
-        {
-            AnsiConsole.MarkupLine($"[bold]{key}:[/] {info[key]}");
-        }
-            
-           // TODO: Make this more esthetic if there is time 
+        
+        var grid = new Grid();
+  
+        grid.AddColumn(new GridColumn { Width = 20, Alignment = Justify.Right });
+        grid.AddColumn(new GridColumn());
+  
+        grid.AddEmptyRow();
+  
+        grid.AddRow(new Markup("[bold]Character Name: [/]"), new Markup(info["Character Name: "]));
+        grid.AddRow(new Markup("[bold]Class: [/]"), new Markup(info["Class: "]));
+        grid.AddRow(new Markup("[bold]Hit Points: [/]"), new Markup(info["Hit Points: "]));
+        grid.AddRow(new Markup("[bold]Prime Requisite: [/]"), new Markup(info["Prime Requisite: "] + ": " + info["Prime requisite score: "] + " (Modifier " + info["Modifier: "] + ")"));
+        grid.AddRow(new Markup("[bold]XP for level 2: [/]"), new Markup( info["XP for level 2: "]));
+        grid.AddRow(
+            new Markup("[bold]Ability Scores: [/]"),
+            new Panel($"[yellow]STR: [/][yellow4]{info["Strength"]} [/]" +
+                      $"[green3]INT: [/][green4]{info["Intelligence"]} [/]" +
+                      $"[blue]WIS: [/][blue1]{info["Wisdom"]} [/]")
+                .NoBorder());
+        grid.AddRow(
+            new Markup(""),
+            new Panel($"[red]DEX: [/][red3]{info["Dexterity"]}[/] " +
+                      $"[darkorange]CON: [/][darkorange3_1]{info["Constitution"]} [/]" +
+                      $"[purple]CHA: [/][purple4]{info["Charisma"]} [/]")
+                .NoBorder());
+  
+        AnsiConsole.Write(grid);
     }
 
 }
