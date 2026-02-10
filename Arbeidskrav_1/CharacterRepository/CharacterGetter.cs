69b3bf2a0506f98869ee3e1c3d6ebcc125144867
kristianeo <olsen.kristiane@gmail.com>
2026-02-10 13:03:21 +0100

    Added DisplayCharacter for CharacterGetter.cs

--- Arbeidskrav_1/CharacterRepository/CharacterGetter.cs
+++ Arbeidskrav_1/CharacterRepository/CharacterGetter.cs
@@ -1,35 +1,39 @@
 using System.Text.Json;
 using Arbeidskrav_1.CharacterClasses;
+using Spectre.Console;
 
 namespace Arbeidskrav_1.CharacterRepository;
 
 public class CharacterGetter
 {
-    public static Dictionary<string, string>? GetCharacter()
+    public static void GetCharacter()
     {
-        Console.WriteLine("Would you like to search for a character? ");
-        string name = Console.ReadLine();
-        const string filePath = @"C:\Users\olsen\OneDrive\Dokumenter\GitHub\Arbeidskrav_1\Arbeidskrav_1\CharacterRepo.json";
-
-        if (!File.Exists(filePath))
-            return null;
-
+        AnsiConsole.MarkupLine("[Blue]Let's search for a character![/]");
+        string name = AnsiConsole.Ask<string>("What is the name of the character?");
+        
+        const string filePath = @"C:\Users\olsen\OneDrive\Dokumenter\GitHub\Arbeidskrav_1\Arbeidskrav_1\CharacterRepository\CharacterRepo.json";
         string json = File.ReadAllText(filePath);
-
-        if (string.IsNullOrWhiteSpace(json))
-            return null;
-
+        
+        if (!File.Exists(filePath) || string.IsNullOrWhiteSpace(json))
+        {
+            throw new FileNotFoundException("The file doesn't exist");
+        }
+        
         List<Dictionary<string, string>>? characters =
             JsonSerializer.Deserialize<List<Dictionary<string, string>>>(json);
 
-        if (characters == null)
-            return null;
-
-        var info = characters.FirstOrDefault(c => c.ContainsValue(name));
-        return info;
+        var info = characters?.FirstOrDefault(c => c.ContainsValue(name));
+        DisplayCharacter(info);
     }
-    
-    
-    
+
+    private static void DisplayCharacter(Dictionary<string, string> info)
+    {
+        foreach (string key in info.Keys)
+        {
+            AnsiConsole.MarkupLine($"[bold]{key}:[/] {info[key]}");
+        }
+            
+           // TODO: Make this more esthetic if there is time 
+    }
 
 }
