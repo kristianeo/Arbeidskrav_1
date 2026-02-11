898f5389b9727b90abd7ec6671e523c5135e17e6
kristianeo <olsen.kristiane@gmail.com>
2026-02-11 20:25:34 +0100

    Added the welcomeMessage to RunGenerator.cs

--- Arbeidskrav_1/Program.cs
+++ Arbeidskrav_1/Program.cs
@@ -1,14 +1,9 @@
-﻿using Arbeidskrav_1.CharacterClasses;
-using Arbeidskrav_1.Generators;
-using Arbeidskrav_1.UserInterface;
-using Spectre.Console;
-
-namespace Arbeidskrav_1;
+﻿namespace Arbeidskrav_1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Welcome.WelcomeMessage();
+        RunGenerator.WelcomeMessage();
     }
 }
--- Arbeidskrav_1/RunGenerator.cs
+++ Arbeidskrav_1/RunGenerator.cs
@@ -3,16 +3,55 @@ using Arbeidskrav_1.CharacterClasses;
 using Arbeidskrav_1.CharacterRepository;
 using Arbeidskrav_1.Generators;
 using Arbeidskrav_1.UserInterface;
+using Spectre.Console;
 
 namespace Arbeidskrav_1;
 
-public class RunGenerator
+public abstract class RunGenerator
 {
+    /// <summary>
+    /// Runs the methods needed to create a character
+    /// </summary>
     public static void Run()
     {
         AbilityScoresGenerator.GenerateAbilityScores();
         AvailableClassesGenerator.AvailableClasses();
         CharacterClass character = CharacterGenerator.GenerateClass(ClassSelector.ChooseClass());
         UserInterface.CharacterDisplayer.DisplayCharacter(character);
+    }
+    
+    /// <summary>
+    /// Shows the starting screen. The player gets to chose if they want to
+    /// make a new character or search in the repository.
+    /// </summary>
+    public static void WelcomeMessage()
+    {
+        Console.Clear();
+        var figlet = new FigletText("Welcome")
+        {
+            Color = Color.Green,
+            Justification = Justify.Center
+        };
+        
+        AnsiConsole.Write(figlet);
+        AnsiConsole.Write(new Rule("[green]to the character generator![/]"));
+        
+        var prompt = new SelectionPrompt<string>()
+            .Title("\n[rapidblink bold]What would you like to do?[/]")
+            .AddChoices("Create character", "Search in character repository");
+        var selected = AnsiConsole.Prompt(prompt);
+        AnsiConsole.MarkupLineInterpolated($"\n[blue]You selected[/] {selected} ");
+        
+        switch (selected)
+        {
+            case "Create character":
+                AnsiConsole.Clear();
+                Run();
+                break;
+            case "Search in character repository":
+                AnsiConsole.Clear();
+                CharacterGetter.GetCharacter();
+                break;
+        }
     }
 }
